bdb28da2faa527ceded9c553f3754a0f0b6f64a4
Esteban Murillo <stbamb@gmail.com>
2019-09-05 17:46:03 -0400

    Fixed problem with cosDistance

--- Assignment1/esteban_murillo_knn.py
+++ Assignment1/esteban_murillo_knn.py
@@ -3,19 +3,21 @@ import math
 _attr_keyword = '@attribute'
 _data_folder = 'KEEL_data'
 
+
 def main():
     #askForInput()
-    k = 15
+    k = 5
     fileName = "titanic-10-fold/titanic-10"
     trainingDataSet = readData(fileName, True)
     testDataSet = readData(fileName, False)
     print("testDataSet:", testDataSet)
     print("trainingDataSet:", trainingDataSet)
-    testDataSetResults = cosDistance1(trainingDataSet, testDataSet)
-    trainingDataSetResults = cosDistance2(trainingDataSet)
-    print("testDataSetResults:", testDataSetResults)
-    print("trainingDataSetResults:", trainingDataSetResults)
-    rightPredictions = nearestNeighbor(trainingDataSetResults, testDataSetResults, k)
+    distances = cosDistance(trainingDataSet, testDataSet)
+    print(len(distances))
+    print(len(distances[0]))
+    print("distances:", distances)
+
+    rightPredictions = nearestNeighbor(distances, k)
     print("rightPredictions:", rightPredictions, "out of", len(testDataSet))
     accuracy = (rightPredictions / len(testDataSet)) * 100
     print("accuracy:", accuracy)
@@ -44,14 +46,13 @@ def readData(fileName, isTrainingData):
     return dataSet
 
 
-def cosDistance1(trainingDataSet, testDataSet):
-    cosSet = []
-
+def cosDistance(trainingDataSet, testDataSet):
+    cosDistances = []
     for i in range(len(testDataSet)):
+        distances = []
         A = testDataSet[i]
         for j in range(len(trainingDataSet)):
             B = trainingDataSet[j]
-            cos = 0
             numerator = 0
             denominator1 = 0
             denominator2 = 0
@@ -61,44 +62,21 @@ def cosDistance1(trainingDataSet, testDataSet):
                 denominator1 += pow(A[k], 2)
                 denominator2 += pow(B[k], 2)
 
-            cos += numerator / (math.sqrt(denominator1) * math.sqrt(denominator2))
-        cosSet.append((cos/len(trainingDataSet), testDataSet[i][-1]))
+            cosDist = numerator / (math.sqrt(denominator1) * math.sqrt(denominator2))
+            distances.append((cosDist, testDataSet[i][-1], trainingDataSet[j][-1]))
+        cosDistances.append(distances)
 
-    return cosSet
-
-
-def cosDistance2(dataSet):
-    cosSet = []
-
-    for i in range(len(dataSet)):
-        A = dataSet[i]
-        for j in range(len(dataSet)):
-            if i == j:
-                continue
-            else:
-                B = dataSet[j]
-                cos = 0
-                numerator = 0
-                denominator1 = 0
-                denominator2 = 0
-
-                for k in range(len(A) - 1):
-                    numerator += (A[k] * B[k])
-                    denominator1 += pow(A[k], 2)
-                    denominator2 += pow(B[k], 2)
-
-                cos += numerator / (math.sqrt(denominator1) * math.sqrt(denominator2))
-        cosSet.append((cos/len(dataSet), dataSet[i][-1]))
-
-    return cosSet
+    return cosDistances
 
 
-def nearestNeighbor(trainingDataSetResults, testDataSetResults, k):
-    distances = calculateDistances(trainingDataSetResults, testDataSetResults, k)
+def nearestNeighbor(distances, k):
     rightPredictions = 0
-    print("distances:", distances)
-    print(len(distances))
 
+    for i in range(len(distances)):
+        distances[i] = mergesort(distances[i])
+        distances[i] = distances[i][0:k]
+    print("distances:", distances)
+    omd = []
     for i in range(len(distances)):
         instance = distances[i]
         expected = instance[0][1]
@@ -112,19 +90,10 @@ def nearestNeighbor(trainingDataSetResults, testDataSetResults, k):
         if count1 > count2:
             rightPredictions += 1
 
+            omd.append(i)
+
+    print("corrected predicted", omd)
     return rightPredictions
-
-
-def calculateDistances(trainingDataSetResults, testDataSetResults, k):
-    distances = []
-    for i in range(len(testDataSetResults)):
-        kDistances = []
-        for j in range(len(trainingDataSetResults)):
-            dist = abs(testDataSetResults[i][0] - trainingDataSetResults[j][0])
-            kDistances.append((dist, testDataSetResults[i][1], trainingDataSetResults[j][1]))
-        sortedDistances = mergesort(kDistances)
-        distances.append(sortedDistances[0:k])
-    return distances
 
 
 def mergesort(arr):
