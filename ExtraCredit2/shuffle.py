69051aeab624ab48d7d96af9d391bd4134c0e5d2
Esteban Murillo <stbamb@gmail.com>
2019-09-13 00:23:27 -0400

    Code for ExtraCredit2 ready

--- ExtraCredit2/shuffle.py
+++ ExtraCredit2/shuffle.py
@@ -1,4 +1,55 @@
+# author        : Esteban M
+# course        : CS-691 Data Mining
+# name          : shuffle.py
+# date          : 20190913
+# usage         : python3 shuffle.py
+# python_version: 3.7
+# notes         : Assignment for extra credit #2
+# description   : Reads data from _input_file_name, shuffles rows
+#                 and then outputs it to _output_file_name
+# ==============================================================================
+
 import random
+
+_input_file_name = "data_all_with_charge.txt"
+_output_file_name = "shuffled_data.txt"
+
+
+def main():
+    lines = readData(_input_file_name)
+
+    if not lines:
+        print("Could not read file")
+        return
+
+    # code just to preserve the first row, which contains the names of the columns
+    first_line = lines[0]
+    del lines[0]
+    shuffled_lines = shuffle(lines)
+
+    # insert the header row back
+    shuffled_lines.insert(0, first_line)
+    writeData(_output_file_name, shuffled_lines)
+
+
+def readData(fileName):
+    try:
+        with open(fileName) as f:
+            lines = f.readlines()
+            f.close()
+    except:
+        return []
+    return lines
+
+
+def writeData(fileName, data):
+    try:
+        f = open(fileName, "w")
+        for i in range(len(data)):
+            f.write(data[i])
+        f.close()
+    except:
+        return "Could not write to file"
 
 
 def shuffle(arr):
@@ -7,6 +58,8 @@ def shuffle(arr):
         temp = arr[random_index]
         arr[random_index] = arr[i]
         arr[i] = temp
-    print(arr)
+    return arr
 
-shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+if __name__ == "__main__":
+    main()
