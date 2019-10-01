adcff1826010f06181f0984627aaf2d7d8885cf0
Esteban Murillo <stbamb@gmail.com>
2019-10-01 02:25:22 -0400

    Moving 'SchoolLevel' class to 'nlp_tools.py' file

--- Assignment3/nlp_tools.py
+++ Assignment3/nlp_tools.py
@@ -6,18 +6,17 @@
 # notes         : Assignment3
 # description   : Needed for esteban_murillo_assignment3.py to run
 # ==============================================================================
+import enum
 import math
 
 from textatistic import Textatistic
-
-from school_level import SchoolLevel
 
 
 def getBookResults(book_data):
     book = Textatistic(book_data[2])
     fres_score = book.flesch_score
     school_level = determineSchoolLevel(fres_score)
-    return "Title: {}\nAuthor: {}\nFlesch reading-ease score: {}\nSchool level: {}".\
+    return "Title: {}\nAuthor: {}\nFlesch reading-ease score: {}\nSchool level: {}". \
         format(book_data[0], book_data[1], fres_score, school_level)
 
 
@@ -37,3 +36,13 @@ def roundup(x):
     elif num > 100:
         num = 100
     return num
+
+
+class SchoolLevel(enum.Enum):
+    COLLEGE_GRADUATE = 30
+    COLLEGE = 50
+    TENTH_TO_TWELFTH_GRADE = 60
+    EIGHTH_TO_NINTH_GRADE = 70
+    SEVENTH_GRADE = 80
+    SIXTH_GRADE = 90
+    FIFTH_GRADE = 100
