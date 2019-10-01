e841f0c41e9b8fcb6f035e827b211e1d772e1c49
Esteban Murillo <stbamb@gmail.com>
2019-09-30 23:16:17 -0400

    Adding skeleton for Assignment3

---
+++ Assignment3/esteban_murillo_assignment3.py
@@ -0,0 +1,20 @@
+# author        : Esteban
+# course        : CS-691 Data Mining
+# name          : esteban_murillo_assignment3.py
+# date          : 20191005
+# usage         : python3 esteban_murillo_assignment3.py
+# python_version: 3.7
+# notes         : Assignment3
+# description   :
+# ==============================================================================
+
+import global_variables as global_vars
+
+
+def main():
+    if global_vars.verbose:
+        print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment3/global_variables.py
@@ -0,0 +1,10 @@
+# author        : Esteban
+# course        : CS-691 Data Mining
+# name          : global_variables.py
+# date          : 20191005
+# python_version: 3.7
+# notes         : Assignment3
+# description   : Needed for esteban_murillo_assignment3.py to run
+# ==============================================================================
+
+verbose = True
