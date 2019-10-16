06a78de361845b1f9777f7c46b504e31c1304f47
Esteban Murillo <stbamb@gmail.com>
2019-10-15 21:01:31 -0400

    Adding skeleton for Assignment4

---
+++ Assignment4/main.py
@@ -0,0 +1,17 @@
+# author        : Esteban
+# course        : CS-691 Data Mining
+# name          : main.py
+# date          : 20191025
+# usage         : python3 main.py
+# python_version: 3.7
+# notes         : Assignment3
+# description   :
+# ==============================================================================
+
+
+def main():
+    print("Nothing has been done yet")
+
+
+if __name__ == "__main__":
+    main()
