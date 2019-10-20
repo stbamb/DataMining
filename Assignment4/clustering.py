fa49b9e6c5737adfdd19801d28a4a47a820229cf
Esteban Murillo <stbamb@gmail.com>
2019-10-20 07:48:57 -0400

    Minor fixes to clustering.py

--- Assignment4/clustering.py
+++ Assignment4/clustering.py
@@ -12,32 +12,37 @@ import config
 
 
 def k_means_clustering(labeled_features, k):
+    features = [feature[0] for feature in labeled_features]
     stop_conditions = []
-    clusters = []
+    new_clusters = []
     iteration = 0
 
     while continueClustering(stop_conditions, iteration):
-        features = [feature[0] for feature in labeled_features]
-        centroids = getCentroids(clusters, features, k, iteration)
-        distances = manhattanDistance(features, centroids)
+        if iteration == 0:  # if it is the first time, then pick random datapoints as centroids
+            old_centroids = getRandomCentroids(features, k)
+        else:  # otherwise compute averages and make it your new centroids
+            old_centroids = new_centroids
+
+        distances = manhattanDistance(features, old_centroids)
         distances_to_clusters = getDistancesToClusters(distances)
-        clusters = assignToCluster(labeled_features, distances_to_clusters)
-        new_centroids = getAvgCentroid(clusters)
-        centroids_changed = didCentroidsChange(centroids, new_centroids)
-        stop_conditions.clear()
-        stop_conditions.append(centroids_changed)
+        old_clusters = new_clusters
+        new_clusters = assignToCluster(labeled_features, distances_to_clusters)
+        new_centroids = getAvgCentroid(new_clusters)
+
+        stop_conditions = (old_clusters != new_clusters, old_centroids != new_centroids)
         iteration += 1
 
         if config.VERBOSE:
             print("*"*128, "ITERATION #", iteration, "*"*128)
             print("Labeled features:\n", labeled_features, "\n")
-            print("Centroids:\n", centroids, "\n")
+            print("Centroids:\n", old_centroids, "\n")
             print("Distances to clusters:\n", distances_to_clusters, "\n")
-            for i in range(len(clusters)):
-                print("Cluster {}, with {} elements:\n {}\n".format(i + 1, len(clusters[i]), clusters[i]))
-            print("Centroids changed?\n", centroids_changed, "\n")
+            for i in range(len(new_clusters)):
+                print("Cluster {}, with {} elements:\n {}\n".format(i + 1, len(new_clusters[i]), new_clusters[i]))
+            print("Clusters changed? {}\nCentroids changed? {}\nIteration: {}\n"
+                  .format(stop_conditions[0], stop_conditions[1], iteration))
 
-    return clusters
+    return new_clusters
 
 
 def continueClustering(conditions, iteration):
@@ -64,26 +69,23 @@ def manhattanDistance(training_data_set, test_data_set):
     return manhattan_distances
 
 
-def getCentroids(clusters, features, k, iteration):
+def getRandomCentroids(features, k):
     centroids = []
-    if iteration == 0:  # if it is the first time, then pick a datapoint at random
-        for i in range(k):
+    for i in range(k):
+        random_centroid_idx = random.randint(0, len(features) - 1)
+        while features[random_centroid_idx] in centroids:
             random_centroid_idx = random.randint(0, len(features) - 1)
-            while features[random_centroid_idx] in centroids:
-                random_centroid_idx = random.randint(0, len(features) - 1)
-            centroids.append(features[random_centroid_idx])
-    else:  # otherwise, calculate the avg
-        centroids = getAvgCentroid(clusters)
+        centroids.append(features[random_centroid_idx])
     return centroids
 
 
 def getAvgCentroid(clusters):
-    avgs = []
+    averages = []
     for cluster in clusters:
         features = [element[0] for element in cluster]
         avg_for_cluster = [sum(value) / len(features) for value in zip(*features)]
-        avgs.append(avg_for_cluster)
-    return avgs
+        averages.append(avg_for_cluster)
+    return averages
 
 
 def getDistancesToClusters(distances):
@@ -106,11 +108,3 @@ def assignToCluster(labeled_features, distances_to_clusters):
         clusters[idx].append(labeled_features[i])
         i += 1
     return clusters
-
-
-def didCentroidsChange(old_centroids, new_centroids):
-    return old_centroids != new_centroids
-
-
-def didClustersChange(old_clusters, new_clusters):
-    return old_clusters != new_clusters
