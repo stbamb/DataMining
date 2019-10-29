04cdf507405bb5ef0635a7e477f129c809b1baca
Esteban Murillo <stbamb@gmail.com>
2019-10-28 23:03:22 -0400

    Adding skeleton for Assignment5

---
+++ Assignment5/main.py
@@ -0,0 +1,17 @@
+# author        : Esteban
+# course        : CS-691 Data Mining
+# name          : main.py
+# date          : 20191108
+# usage         : python3 main.py
+# python_version: 3.7
+# notes         : Assignment5
+# description   :
+# ==============================================================================
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
